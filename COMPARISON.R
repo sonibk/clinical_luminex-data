30c01b526681aefb44a259b06864a8aea51dadba
Brenda Karumbo <67633694+sonibk@users.noreply.github.com>
2023-07-07 16:49:27 +0100

    Update COMPARISON.R

--- COMPARISON.R
+++ COMPARISON.R
@@ -36,18 +36,19 @@ parametric_variables <- subset(norm.test, p > 0.05)
 
 ### carry out the paired T-TEST OR WILCOXON test based on the results from normality testing
 ggpaired(ADM_24, x = "Time.point", y = "MIP1.alpha",
-         color = "Time.point", line.color = "gray", line.size = 0.4,
+         color = "Time.point", line.color = "darkgray", line.size = 0.4,
          palette = "jco")+
   stat_compare_means(paired = TRUE, label = "p.signif", label.y = 1000)+
   facet_wrap(~ Category, ncol = 2)+
   labs(x = "Time", y = "MIP1 alpha", title = "MIP1.alpha")+
   theme(legend.position = "none",
         strip.text = element_text(face = "bold", size = 15),
-        axis.title.x = element_text(size = 16),
-        axis.text.x = element_text(size =  12),
-        axis.text.y = element_text(size =  12),
-        axis.title.y = element_text(size = 16),
+        axis.title.x = element_text(size = 16, face = "bold"),
+        axis.text.x = element_text(size =  12,face = "bold"),
+        axis.text.y = element_text(size =  12, face = "bold"),
+        axis.title.y = element_text(size = 16,face = "bold"),
         plot.title = element_text(size = 22, face = "bold",hjust = 0.5))
+
 
 ###sit back relax and enjoy looking at your results :)
 
