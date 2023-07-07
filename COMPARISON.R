9c238a13eae25be419852359630876bd50015d63
Brenda Karumbo <67633694+sonibk@users.noreply.github.com>
2023-07-07 16:34:22 +0100

    Update COMPARISON.R

--- COMPARISON.R
+++ COMPARISON.R
@@ -12,7 +12,7 @@ setwd("C:/Users/Yp/Desktop/luminex/FOR USE")
 
 
 ####load the csv
-### note this CSV only contains data for samples with matching 
+### note this CSV only contains data for samples with matching admmission and 24hrs data 
 ADM_24 <- read.csv("Adm_24hrs data.csv", header = T)
 
 
@@ -32,10 +32,11 @@ norm.test <- ADM_24 %>% shapiro_test(parameters)
 non_parametric_variables <- subset(norm.test , p < 0.05)
 parametric_variables <- subset(norm.test, p > 0.05)
 
-###note for the parametric use paired t-test(different measurements from same samples)
+#### This is a pairwise comparison as they are different measurements of the same samples
+###note for the parametric use paired t-test
 ###for the non-parametric use a paired wilcoxon test
 
-
+### carry out the paired T-TEST OR WILCOXON test based on the results from normality testing
 ggpaired(ADM_24, x = "Time.point", y = "MIP1.alpha",
          color = "Time.point", line.color = "gray", line.size = 0.4,
          palette = "jco")+
@@ -49,6 +50,10 @@ ggpaired(ADM_24, x = "Time.point", y = "MIP1.alpha",
         axis.text.y = element_text(size =  12),
         axis.title.y = element_text(size = 16),
         plot.title = element_text(size = 22, face = "bold",hjust = 0.5))
+
+###sit back relax and enjoy looking at your results :)
+
+
 
 
 
